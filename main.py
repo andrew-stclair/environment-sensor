9241d6129d1292fe353b93e0b6e5db69142e1005
Andrew St Clair <andrew_stclair95@hotmail.com>
2025-01-16 22:35:10 +1100

    Extra print statements

--- main.py
+++ main.py
@@ -11,36 +11,47 @@ def main():
   ready = False
   server = False
 
-  registry = CollectorRegistry(namespace='environ_sensor')
+  print("Setting up the registry")
+  registry = CollectorRegistry(namespace="environ_sensor")
 
-  metric_humidity = Gauge('humidity', 'Humidity (Relative)', registry=registry)
-  metric_pressure = Gauge('pressure', 'Barometric Pressure (Pascals)', registry=registry)
-  metric_temperature = Gauge('temperature', 'Temperature (Celsius)', registry=registry)
+  print("Setting up the Gauges")
+  metric_humidity = Gauge("humidity", "Humidity (Relative)", registry=registry)
+  metric_pressure = Gauge("pressure", "Barometric Pressure (Pascals)", registry=registry)
+  metric_temperature = Gauge("temperature", "Temperature (Celsius)", registry=registry)
 
+  print("Setting up the router")
   router = Router()
-  router.register('GET', '/metrics', registry.handler)
+  router.register("GET", "/metrics", registry.handler)
 
   sensor = PiicoDev_BME280()
 
+  print("Getting ready")
   while not ready:
     time.sleep(0.5)
     ready = wlan.active()
 
+  print("Starting the server")
   while True:
     while not server:
+      print("No server yet")
       server = start_http_server(8080, address=wlan.ifconfig()[0])
+    print("Server ready")
 
+    print("Getting the values")
     temperature, pressure, humidity = sensor.values() 
 
+    print("Setting the Gauges")
     metric_humidity.set(humidity)
     metric_pressure.set(pressure)
     metric_temperature.set(temperature)
 
+    print("Trying to accept a connection")
     try:
       server.accept(router)
+      print("Accepted a connection")
     except OSError as err:
-      print('Error accepting request: {}'.format(err))
+      print("Error accepting request: {}".format(err))
     except ValueError as err:
-      print('Error parsing request: {}'.format(err))
+      print("Error parsing request: {}".format(err))
 
 main()
